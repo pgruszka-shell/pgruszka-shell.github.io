e157d7c92b451c24961ba978ee29f721e39d2c72
Rajat Chauhan <Rajat.Chauhan@shell.com>
2024-02-14 14:53:24 +0530

    B2B Card Encryption

--- js/b2b.js
+++ js/b2b.js
@@ -6,15 +6,25 @@ function generatePublicKey(modulusB64, exponentB64) {
     return rsaKey;
   }
 
-  function encrypt(elementId) {
-    const plaintext = document.getElementById(elementId).value;
+  function encrypt() {
+    const card_number = document.getElementById("card_number_text").value;
+    
     const rsaKey = generatePublicKey(
       "4ieVH3h6KCdi8SmqkkAqnpkRRmtUcE3m0iQKVx9WrBa05HxORANxfi+Y+K3YCS36az3mJS4pHVli66nCdqzmjXCLNNQZRimgY826NvhI/VxkWCV32oXYstDh/XmF75CzHOFvXbnkmDqNs1BIiZurf9aYTgVjMfkHjU96YODgipT2uwlUvb/0b3XGWJT0MW5pZ/60JBjesuS9LAFApUkn//Y2zA62U3xOyBejgeassjvs7THY4m5YnD+/0zXsQOQKG1gxwA84wjxFdBMoogf3NFkPBUUDUYPeKB2bTCsxWGoMB3cspVvdKcPDmPYejOQbjbfi4OiV76VfPELy/0d5fQ==",
       "AQAB"
     );
-    const encryptedData = rsaKey.encrypt(plaintext);
-    const encryptedDataB64 = hex2b64(encryptedData);
-    document.getElementById("encryptedDataText").value = encryptedDataB64;
+    const encrypted_card_number_data = rsaKey.encrypt(card_number);
+    const encrypted_card_number_data_B64 =  hex2b64(encrypted_card_number_data);
+
+    const card_expiry = document.getElementById("card_expiry_text").value;
+    const encrypted_card_expiry_data = rsaKey.encrypt(card_expiry);
+    const encrypted_card_expiry_data_B64 = hex2b64(encrypted_card_expiry_data) ;
+    
+    const encryptedDataB64 = {
+            "encryptedCardNumber": encrypted_card_number_data_B64,
+            "encryptedExpiryDate": encrypted_card_expiry_data_B64
+    }
+    document.getElementById("encryptedDataText").value = JSON.stringify(encryptedDataB64);
   }
 
   function copyText(){
